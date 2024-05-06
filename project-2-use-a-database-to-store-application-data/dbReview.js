017893b79d53575beca11f1d85e22d0f3acf5614
joeyst <joeystenbeck@gmail.com>
2024-05-05 23:32:08 -0700

    Project 2: WIP.

---
+++ project-2-use-a-database-to-store-application-data/dbReview.js
@@ -0,0 +1,38 @@
+const { Review } = require('./mongoose_.js')
+
+// function errorCb(err) {
+//   if (!(err)) {
+//     console.log('User saved!')
+//   }
+// }
+
+function postReview(req, res) {
+  new Review(req.body).save(err => {
+    if (err) {
+      res.status(400).send()
+    } else {
+      console.log("Review saved!")
+      res.status(200).send()
+    }
+  })
+}
+
+async function getReview(req, res) {
+  res.send(await Review.findById(req.params.reviewId))
+}
+
+async function putReview(req, res) {
+  res.send(await Review.findByIdAndUpdate(req.params.reviewId, req.body, { new: true }))
+}
+
+async function deleteReview(req, res) {
+  await Review.findByIdAndDelete(req.params.reviewId)
+  res.send("Review deleted!")
+}
+
+module.exports = {
+  postReview: postReview,     // /
+  getReview: getReview,       // /:reviewID
+  putReview: putReview,       // /:reviewID
+  deleteReview: deleteReview, // /:reviewID
+}
