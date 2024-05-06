00332d0b65837b7c4a8e56d9b32abc3cfda76697
joeyst <joeystenbeck@gmail.com>
2024-05-06 00:22:45 -0700

    Project 2: Remove old user logic.

--- project-2-clone/osu-cs493-assignment-2/api/users.js
+++ project-2-clone/osu-cs493-assignment-2/api/users.js
@@ -4,42 +4,23 @@ const crud = require('../db/crud.js')
 
 exports.router = router;
 
-const { businesses } = require('./businesses');
-const { reviews } = require('./reviews');
-const { photos } = require('./photos');
-
 /*
  * Route to list all of a user's businesses.
  */
 router.get('/:userid/businesses', function (req, res) {
-  const userid = parseInt(req.params.userid);
-
   res.send(crud.getUserBusinessList(req, res))
-
-  const userBusinesses = businesses.filter(business => business && business.ownerid === userid);
-  res.status(200).json({
-    businesses: userBusinesses
-  });
 });
 
 /*
  * Route to list all of a user's reviews.
  */
 router.get('/:userid/reviews', function (req, res) {
-  const userid = parseInt(req.params.userid);
-  const userReviews = reviews.filter(review => review && review.userid === userid);
-  res.status(200).json({
-    reviews: userReviews
-  });
+  res.send(crud.getUserReviewList(req, res))
 });
 
 /*
  * Route to list all of a user's photos.
  */
 router.get('/:userid/photos', function (req, res) {
-  const userid = parseInt(req.params.userid);
-  const userPhotos = photos.filter(photo => photo && photo.userid === userid);
-  res.status(200).json({
-    photos: userPhotos
-  });
+  res.send(crud.getUserPhotoList(req, res))
 });
