67cbe366780bd8e294f6904f63dbefeed3a957e2
joeyst <joeystenbeck@gmail.com>
2024-05-06 17:56:34 -0700

    Project 2: Remove dead User CRUD functions.

--- project-2-clone/osu-cs493-assignment-2/db/user.js
+++ project-2-clone/osu-cs493-assignment-2/db/user.js
@@ -12,37 +12,7 @@ async function getUserPhotoList(req, res) {
   res.send(await Photo.find({userid: req.params.userid}))
 }
 
-/*
-async function postUser(req, res) {
-  new User(req.body).save(err => {
-    if (err) {
-      res.status(400).send()
-    } else {
-      console.log("User saved!")
-      res.status(200).send()
-    }
-  })
-}
-
-async function getUser(req, res) {
-  res.send(await User.findById(req.params.userId))
-}
-
-async function putUser(req, res) {
-  res.send(await User.findByIdAndUpdate(req.params.userId, req.body, { new: true }))
-}
-
-async function deleteUser(req, res) {
-  await User.findByIdAndDelete(req.params.userId)
-  res.send("User deleted!")
-}
-*/
-
 module.exports = {
-  // postUser: postUser,     // /
-  // getUser: getUser,       // /:UserID
-  // putUser: putUser,       // /:UserID
-  // deleteUser: deleteUser, // /:UserID
   getUserBusinessList: getUserBusinessList,
   getUserReviewList: getUserReviewList,
   getUserPhotoList: getUserPhotoList,
