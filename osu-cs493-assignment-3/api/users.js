7f7d47c1670ae35c552918ffafb7e096781276b5
joeyst <joeystenbeck@gmail.com>
2024-05-19 04:38:29 -0700

    Project 3: Add POST /users.

--- osu-cs493-assignment-3/api/users.js
+++ osu-cs493-assignment-3/api/users.js
@@ -6,15 +6,27 @@ const { Review } = require('../models/review')
 
 const router = Router()
 
+const secret_key = process.env.APP_SECRET_KEY;
+
 /*
  * TODO: Add POST /users with provided name, email address, and password, and salted and hashed password on server before storing it.
  */
-router.get('/:userId/businesses', async function (req, res) {
-  const userId = req.params.userId
-  const userBusinesses = await Business.findAll({ where: { ownerId: userId }})
-  res.status(200).json({
-    businesses: userBusinesses
-  })
+router.post('/', async function (req, res) {
+  const { id, name, email, password, admin } = req.params
+  const user = await User.find({ where: { email: email }})
+
+  if (user == null) {
+    res.status(401).send(`Unable to find email ${email}`)
+    return
+  }
+
+  if (!user.validPassword(password)) {
+    res.status(401).send(`Unable to find password ${password}`)
+    return
+  }
+
+  const token = jwt.sign({ id: id, name: name, email: email }, secret_key)
+  res.status(200).json({token})
 })
 
 /*
