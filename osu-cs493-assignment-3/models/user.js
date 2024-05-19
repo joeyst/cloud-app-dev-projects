370d1434132c49c07c4d88aa8e55d75757d8d410
joeyst <joeystenbeck@gmail.com>
2024-05-19 04:02:57 -0700

    Project 3: Add User model.

---
+++ osu-cs493-assignment-3/models/user.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize')
+
+const sequelize = require('../lib/sequelize')
+
+const User = sequelize.define('user', {
+  name: { type: DataTypes.TEXT, allowNull: false },
+  email: { type: DataTypes.TEXT, allowNull: false, unique: true }, 
+  password: { type: DataTypes.TEXT, set(value) {
+    this.setDataValue('password', hash(value))
+  },
+  admin: { type: DataTypes.BOOLEAN, allowNull: false }
+}})
+
+exports.User = User
+exports.UserClientFields = [
+  'name',
+  'email',
+  'password',
+  'admin'
+]
