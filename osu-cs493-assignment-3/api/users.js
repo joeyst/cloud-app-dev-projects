ac10b1f2d23fd116a693095aecd7152d78ad35ff
joeyst <joeystenbeck@gmail.com>
2024-05-19 17:38:18 -0700

    Project 3: Copy in requireAuthentication.

--- osu-cs493-assignment-3/api/users.js
+++ osu-cs493-assignment-3/api/users.js
@@ -9,6 +9,23 @@ const router = Router()
 
 const secret_key = process.env.APP_SECRET_KEY;
 
+function requireAuthentication(req, res, next) {
+    // Get the token from the request
+    const auth_header = req.get('Authorization') || '';
+    const header_parts = auth_header.split(' ');
+
+    const token = header_parts[0] == "Bearer"? header_parts[1]: null;
+
+    try {
+        const payload = jwt.verify(token, secret_key);
+        req.user = payload.sub;
+        next();
+
+    } catch (err) {
+        res.status(401).json({"error": "invalid token"});
+    }
+}
+
 /*
  * Route to create new account from user name, email, and password. 
  */
