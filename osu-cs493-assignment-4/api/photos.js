601cc3135f3ec22e7197dc38f0a2b1c3b0d93827
joeyst <joeystenbeck@gmail.com>
2024-06-02 23:21:41 -0700

    Project 4: Remove old code.

--- osu-cs493-assignment-4/api/photos.js
+++ osu-cs493-assignment-4/api/photos.js
@@ -50,46 +50,13 @@ router.post('/', upload.single('file'), async (req, res) => {
       err: "Request body needs'image' file and 'userId'."
     });
   }
-  if (validateAgainstSchema(req.body, PhotoSchema)) {
-    try {
-      const id = await insertNewPhoto(req.body)
-      res.status(201).send({
-        id: id,
-        links: {
-          photo: `/photos/${id}`,
-          business: `/businesses/${req.body.businessId}`
-        }
-      })
-    } catch (err) {
-      console.error(err)
-      res.status(500).send({
-        error: "Error inserting photo into DB.  Please try again later."
-      })
-    }
-  } else {
-    res.status(400).send({
-      error: "Request body is not a valid photo object"
-    })
-  }
 })
 
 /*
  * GET /photos/{id} - Route to fetch info about a specific photo.
  */
 router.get('/:id', async (req, res, next) => {
-  try {
-    const photo = await getPhotoById(req.params.id)
-    if (photo) {
-      res.status(200).send(photo)
-    } else {
-      next()
-    }
-  } catch (err) {
-    console.error(err)
-    res.status(500).send({
-      error: "Unable to fetch photo.  Please try again later."
-    })
-  }
+  
 })
 
 module.exports = router
