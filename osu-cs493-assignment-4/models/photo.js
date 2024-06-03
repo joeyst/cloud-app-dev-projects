400bd42e51aaa78dc66b58c35b3570e968aa6205
joeyst <joeystenbeck@gmail.com>
2024-06-02 23:19:09 -0700

    Project 4: Remove functions.

--- osu-cs493-assignment-4/models/photo.js
+++ osu-cs493-assignment-4/models/photo.js
@@ -17,67 +17,12 @@ const PhotoSchema = {
 }
 exports.PhotoSchema = PhotoSchema
 
-exports.saveImageInfo = async function (image) {
+exports.saveImageInfo = async (image) => {
   const db = getDbReference();
   const collection = db.collection('photos');
   const result = await collection.insertOne(image);
   return result.insertedId;
 }
-
-/*
- * Executes a DB query to insert a new photo into the database.  Returns
- * a Promise that resolves to the ID of the newly-created photo entry.
- */
-async function insertNewPhoto(photo) {
-  console.log(`RECEIVED IN insertNewPhoto: ${JSON.stringify(photo)}`)
-  photo = extractValidFields(photo, PhotoSchema)
-  
-  const db = getDbReference();
-  const bucket = new GridFSBucket(db, { bucketName: 'photos' });
-
-  const metadata = {
-    businessId: photo.businessId,
-    caption: photo.caption,
-    file: photo.file
-  };
-
-  const uploadStream = bucket.openUploadStream(
-    photo.file.filename,
-    { metadata: metadata }
-  );
-
-  fs.createReadStream(photo.path)
-      .pipe(uploadStream)
-      .on('error', (err) => {
-        reject(err);
-      })
-      .on('finish', (result) => {
-        resolve(result._id);
-      });
-
-  return result.insertedId
-}
-exports.insertNewPhoto = insertNewPhoto
-
-/*
- * Executes a DB query to fetch a single specified photo based on its ID.
- * Returns a Promise that resolves to an object containing the requested
- * photo.  If no photo with the specified ID exists, the returned Promise
- * will resolve to null.
- */
-async function getPhotoById(id) {
-  const db = getDbReference()
-  const bucket = new GridFSBucket(db, { bucketName: 'photos' })
-  if (!ObjectId.isValid(id)) {
-    return null
-  } else {
-    const results = await bucket
-      .find({ _id: new ObjectId(id) })
-      .toArray()
-    return results[0]
-  }
-}
-exports.getPhotoById = getPhotoById
 
 exports.getImageInfoById = async (id) => {
   const db = getDbReference();
