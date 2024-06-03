1744243b2ea4923f3ce2b42771652e7e77ec12eb
joeyst <joeystenbeck@gmail.com>
2024-06-02 23:12:45 -0700

    Project 4: Add multer filename callback.

--- osu-cs493-assignment-4/api/photos.js
+++ osu-cs493-assignment-4/api/photos.js
@@ -16,6 +16,11 @@ const multer = require('multer')
 const imageTypes = ["image/jpeg", "image/png"]
 const upload = multer({
   storage: multer.diskStorage({ destination: `${__dirname}/uploads` }),
+  filename: (req, file, callback) => { 
+    const filename = crypto.pseudoRandomBytes(16).toString('hex');
+    const extension = imageTypes[file.mimetype];
+    callback(null, `${filename}.${extension}`);
+  },
   fileFilter: (req, file, callback) => {
     callback(null, !!imageTypes[file.mimetype]);
   }
