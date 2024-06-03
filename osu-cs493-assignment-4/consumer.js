fd497ec527cae5acc1dd970d091d641cfe358295
joeyst <joeystenbeck@gmail.com>
2024-06-02 23:52:32 -0700

    Project 4: Compile worker code from last part of Exploration 9.

--- osu-cs493-assignment-4/consumer.js
+++ osu-cs493-assignment-4/consumer.js
@@ -18,3 +18,20 @@ async function main() {
   }
 }
 main();
+
+/* Worker code */ 
+const sizeOf = require('image-size');
+
+const id = msg.content.toString();
+const downloadStream = getDownloadStreamById(id);
+
+const imageData = [];
+downloadStream.on('data', (data) => {
+  imageData.push(data);
+});
+
+downloadStream.on('end', async () => {
+  const dimensions = sizeOf(Buffer.concat(imageData));
+  const result = await updateImageSizeById(id, dimensions);
+});
+
