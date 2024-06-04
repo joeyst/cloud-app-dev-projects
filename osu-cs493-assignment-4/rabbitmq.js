77342df426852d6807f06a4d1bddb22cf22a3552
joeyst <joeystenbeck@gmail.com>
2024-06-04 11:32:02 -0700

    Project 4: Fix rabbitmq.js exporting.

--- osu-cs493-assignment-4/rabbitmq.js
+++ osu-cs493-assignment-4/rabbitmq.js
@@ -2,11 +2,16 @@ const amqp = require('amqplib');
 const rabbitmqHost = process.env.RABBITMQ_HOST;
 const rabbitmqUrl = `amqp://${rabbitmqHost}`;
 
-export function getChannel() {
+function getChannel() {
   const connection = await amqp.connect(rabbitmqUrl);
   return await connection.createChannel();
 }
 
-export function sendIdToQueue(id) {
+function sendIdToQueue(id) {
   getChannel().sendToQueue('images', Buffer.from(id.toString()))
+}
+
+module.exports = {
+  getChannel,
+  sendIdToQueue,
 }
